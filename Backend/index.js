b71b07721548b6d92b5c8f94362f833d14c6e440
Avishek Singh <115368094+AviSingh017@users.noreply.github.com>
2023-07-05 13:21:11 +0530

    Backend Deploy for NodeApis App

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const {connection} = require("./config/db");
 require("dotenv").config();
-
+const cors = require("cors");
 const {userRoute} = require("./routes/user.route");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/",(req,res)=>{
     res.send("Hello world!");
 });
-
 app.use("/user", userRoute);
 
 app.listen(process.env.port, async()=>{
